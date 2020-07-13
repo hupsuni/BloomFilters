69c495a6bfd1a666f3419574d62c17b7e8d7ccad
Nick Huppert <s3729119@student.rmit.edu.au>
2020-07-13 14:30:01 +1000

    Updating testing suite

--- bloom_test.py
+++ bloom_test.py
@@ -10,32 +10,19 @@ from Random_IBLT.random_iblt import RIBLT
 class BloomTest:
 
     DEFAULT_TEST_SIZE = 1000000
-    DEFAULT_TABLE_SIZE = .75
-    DEFAULT_TABLE1_DIFFERENCE = .2
-    DEFAULT_TABLE2_DIFFERENCE = .2
+    DEFAULT_TABLE_SIZE = .6
+    DEFAULT_SYMMETRIC_DIFFERENCE = .4
 
     @staticmethod
-    def test(test_set_size=DEFAULT_TEST_SIZE, table_size=DEFAULT_TABLE_SIZE,
-             table1_unique=DEFAULT_TABLE1_DIFFERENCE, table2_unique=DEFAULT_TABLE2_DIFFERENCE):
-        test_items1 = []
-        test_items2 = []
-        start_time = 0
-        end_time = 0
-        try:
-            with open("test_data.txt", "a") as test_data:
-                test_data.write("\n%s\n" % datetime.now())
-        except (FileNotFoundError, FileExistsError) as error:
-            print("%s" % error)
+    def test(test_items1, test_items2, symmetric_difference, table_size=DEFAULT_TABLE_SIZE):
+
+        if len(test_items1) != len(test_items2):
             return
 
-        # Generate test sets
-        for i in range(test_set_size):
-            test_items1.append(i)
-        for i in range(int(test_set_size*table1_unique), int(test_set_size+test_set_size*table2_unique)):
-            test_items2.append(i)
-
-        size = int(table_size*test_set_size)
+        test_set_size = len(test_items1)
+        size = int(test_set_size * table_size)
         bloom_table = IBloomLT(m=size)
+
         # Test standard IBLT
         start_time = time()
         bloom1 = bloom_table.generate_table(test_items1)
@@ -52,13 +39,8 @@ class BloomTest:
         print(result[2])
         average_table_size = (asizeof.asizeof(bloom1) + asizeof.asizeof(bloom2)) / 2
 
-        with open("test_data.txt", "a") as test_data:
-            test_data.write("IBLT: Data size: %s |Table size: %s |Symmetric difference: %s "
-                            "|Table average creation time: %s |Decoding time: %s |Decoding %s \n"
-                            % (test_set_size, table_size * test_set_size,
-                               test_set_size * (table1_unique + table2_unique),
-                               table_creation_time, comparison_time, result[2]))
-
+        BloomTest.write_to_file("Normal IBLT", test_set_size, table_size, symmetric_difference,
+                                average_table_size, table_creation_time, comparison_time, result[2])
         # Test Random IBLT
         key = randint(1, 1000)
         start_time = time()
@@ -75,15 +57,44 @@ class BloomTest:
         comparison_time = end_time - start_time
         print(result[2])
         average_table_size = (asizeof.asizeof(bloom1) + asizeof.asizeof(bloom2)) / 2
+        BloomTest.write_to_file("Random IBLT", test_set_size, table_size, symmetric_difference,
+                                average_table_size, table_creation_time, comparison_time, result[2])
 
+    @staticmethod
+    def generate_test_set(size=DEFAULT_TEST_SIZE, symmetric_difference=DEFAULT_SYMMETRIC_DIFFERENCE):
+        test_items1 = []
+        test_items2 = []
+        for i in range(size):
+            test_items1.append(i)
+        for i in range(int(size * (symmetric_difference / 2)),
+                       int(size + size * (symmetric_difference / 2))):
+            test_items2.append(i)
+
+        return test_items1, test_items2
+
+    @staticmethod
+    def write_to_file(test_type, test_set_size, table_size, symmetric_difference, average_table_size,
+                      table_creation_time, comparison_time, result):
         with open("test_data.txt", "a") as test_data:
-            test_data.write("Random Hash IBLT: Data size: %s |Table size: %s |Symmetric difference: %s "
-                            "|Table average creation time: %s |Decoding time: %s |Decoding %s \n"
-                            % (test_set_size, table_size * test_set_size,
-                               test_set_size * (table1_unique + table2_unique),
-                               table_creation_time, comparison_time, result[2]))
+            test_data.write("%s: Data size: %s |Table size: %s |Symmetric difference: %s "
+                            "|Table average creation time: %s |Average table byte size: %s "
+                            "|Decoding time: %s |Decoding %s \n"
+                            % (test_type, test_set_size, table_size * test_set_size,
+                               symmetric_difference, average_table_size,
+                               table_creation_time, comparison_time, result))
 
 
 if __name__ == "__main__":
-    for i in range(100):
-        BloomTest.test(test_set_size=400000)
+    difference = .25
+    test_data1, test_data2 = BloomTest.generate_test_set(100000, difference)
+
+    # Test table sizes from 60% to 80%
+    for i in range(60, 81):
+        for j in range(10):
+            BloomTest.test(test_data1, test_data2, symmetric_difference=difference, table_size=i/100)
+
+    # Test symmetric differences from 25% to 50% on static size
+    for i in range(25, 50):
+        test_data1, test_data2 = BloomTest.generate_test_set(100000, i/100)
+        for j in range(10):
+            BloomTest.test(test_data1, test_data2, symmetric_difference=difference, table_size=.7)
