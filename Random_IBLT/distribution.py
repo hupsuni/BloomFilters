6781adba171c27488c9274dfe9ef44d50442fa5b
Nick Huppert <s3729119@student.rmit.edu.au>
2020-07-21 15:30:51 +1000

    Testing possible algos

--- Random_IBLT/distribution.py
+++ Random_IBLT/distribution.py
@@ -5,7 +5,7 @@ import math
 class Distribution:
 
     @staticmethod
-    def create_possion_distribution(minimum, average):
+    def create_poisson_distribution(minimum, average):
         distributions = []
         distribution = 0
         if minimum >= max or minimum >= average:
@@ -16,8 +16,31 @@ class Distribution:
 
         return distributions
 
+    @staticmethod
+    def create_aloha_style_distribution(x, a, n):
+        num = []
+        for i in range(2, n+1):
+            top = math.pow(x, i)/(i*(i-1))
+            top = top - (a*math.pow(x, 2)/2)
+
+            bot = 1/(i*(i-1))
+            bot = bot - (a/2)
+            num.append((i, top, bot))
+
+        return num
+
+    @staticmethod
+    def create_robust_soliton_distribution():
+        pass
+
 
 if __name__ == "__main__":
-    dist = Distribution.create_possion_distribution(2, 6)
+    # dist = Distribution.create_poisson_distribution(2, 6)
+    dist = Distribution.create_aloha_style_distribution(4, .1, 7)
+    sum1 = 0
+    sum2 = 0
     for item in dist:
         print(item)
+        sum1 += item[1]
+        sum2 += item[2]
+    print(1/sum1/sum2)
