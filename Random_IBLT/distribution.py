42f87f3b9816ea5efa3efd131dfe9a4f3b3d0b81
Nick Huppert <s3729119@student.rmit.edu.au>
2020-07-22 22:06:24 +1000

    ???

--- Random_IBLT/distribution.py
+++ Random_IBLT/distribution.py
@@ -8,9 +8,9 @@ class Distribution:
     def create_poisson_distribution(minimum, average):
         distributions = []
         distribution = 0
-        if minimum >= max or minimum >= average:
+        if minimum >= average:
             return distributions
-        for k in range(0, int((average - minimum) * 4)):
+        for k in range(0, (int(average-minimum) * 2) + 2):
             distribution += (math.pow((average - minimum), k) * math.exp(-(average - minimum))) / (math.factorial(k))
             distributions.append((distribution, minimum + k))
 
@@ -19,13 +19,25 @@ class Distribution:
     @staticmethod
     def create_aloha_style_distribution(x, a, n):
         num = []
-        for i in range(2, n+1):
-            top = math.pow(x, i)/(i*(i-1))
-            top = top - (a*math.pow(x, 2)/2)
+        for N in range(1, n+1):
+            bot = 0
+            top = 0
+            for i in range(2, N+1):
+                top += math.pow(x, i)/(i*(i-1)) - (a*math.pow(x, 2)/2)
 
-            bot = 1/(i*(i-1))
-            bot = bot - (a/2)
-            num.append((i, top, bot))
+                bot += 1/(i*(i-1)) - (a/2)
+
+            y = math.pow(x, 2) / (2 * (2 - 1)) - (a * math.pow(x, 2) / 2)
+            z = 1 / (2 * (2 - 1)) - (a / 2)
+
+            top2 = 0
+            bot2 = 0
+            for i in range(1, n+1):
+                top2 += (math.pow(x, i)/i) - (a*x)
+                bot2 += 1/i
+            px = math.exp(-((bot2-a)*(1-x)))
+            bot2 -= a
+            num.append((x, top/bot, y/z, top2/bot2, px))
 
         return num
 
@@ -35,12 +47,9 @@ class Distribution:
 
 
 if __name__ == "__main__":
-    # dist = Distribution.create_poisson_distribution(2, 6)
-    dist = Distribution.create_aloha_style_distribution(4, .1, 7)
+    # dist = Distribution.create_poisson_distribution(2, 7)
+    dist = Distribution.create_aloha_style_distribution(2, .01, 5)
     sum1 = 0
     sum2 = 0
     for item in dist:
         print(item)
-        sum1 += item[1]
-        sum2 += item[2]
-    print(1/sum1/sum2)
